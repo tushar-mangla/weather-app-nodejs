fca50c10aa7a5e2e0beda31ef6d51a28b6960ed4
Tushar Mangla <tushar.mangla1120@gmail.com>
2021-06-21 13:31:55 +0530

    change https to http

--- src/utilis/geocode.js
+++ src/utilis/geocode.js
@@ -2,7 +2,7 @@ const request = require("request");
 
 const geocode = (address, callback) => {
   const url =
-    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    "http://api.mapbox.com/geocoding/v5/mapbox.places/" +
     encodeURIComponent(address) +
     ".json?access_token=pk.eyJ1IjoidHVzaGFyLTQ1NiIsImEiOiJja3BwYm5maW4wMG01MnZtb29rZnRsaThqIn0.DUqrpMRd4A1jQsvuuCIdVQ&limit=1";
 
